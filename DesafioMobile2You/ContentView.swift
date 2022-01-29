bd4897092ffba76a477ff1dea2c98327940964af
Igor Silva <51680417+igorsilvadev@users.noreply.github.com>
2022-01-29 12:11:28 -0300

    Initial Commit

---
+++ DesafioMobile2You/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesafioMobile2You
+//
+//  Created by Igor Samoel da Silva on 29/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesafioMobile2You/DesafioMobile2YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioMobile2YouApp.swift
+//  DesafioMobile2You
+//
+//  Created by Igor Samoel da Silva on 29/01/22.
+//
+
+import SwiftUI
+
+@main
+struct DesafioMobile2YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
